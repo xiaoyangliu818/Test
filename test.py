6e56bfb849a5a60fdbe3d0864fc893b3c8af1798
Xiaoyang <xiaoyang.liu.1@stonybrook.edu>
2020-05-17 22:04:05 -0400

    'test.py'

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 17 21:55:09 2020
+
+@author: xiaoyangliu
+"""
+
+from skimage import io
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+
+for i in range(10):
+    print(i)
